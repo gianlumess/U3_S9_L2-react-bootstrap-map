ad316b06755a40b596c84f5da620d661be687edb
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-06-25 17:07:32 +0200

    fixato footer

--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -3,17 +3,15 @@ import { Component } from "react";
 class MyFooter extends Component {
   render() {
     return (
-      <div className="text-center bg-dark text-white">
+      <div className="text-center bg-dark text-white p-4">
         <h2>Contattaci</h2>
         <div id="contatti">
           <a href="tel: +3911223456">(+39) 11223456</a>
           <br />
           <a href="mailto: info@epicode.school">info@epicode.school</a>
           <br />
-          <address>Via Appia 1 - 00100 Roma (RM)</address>
+          <address className="mb-0">Via Appia 1 - 00100 Roma (RM)</address>
         </div>
-        <hr />
-        <p className="mb-0">&copy; 2024</p>
       </div>
     );
   }
