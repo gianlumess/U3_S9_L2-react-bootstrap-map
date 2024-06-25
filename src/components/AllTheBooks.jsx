d9a009f75cc4ebb0a70ff692faa6eb8febe0afeb
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-06-25 23:03:26 +0200

    cambiamento di card dinamico al click dei bottoni utilizzando setState()

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -10,32 +10,34 @@ import ToggleButton from "react-bootstrap/ToggleButton";
 import ToggleButtonGroup from "react-bootstrap/ToggleButtonGroup";
 
 class AllTheBooks extends Component {
-  state = {};
+  state = {
+    category: fantasy,
+  };
   render() {
     return (
       <>
         <ToggleButtonGroup className="mt-3 mb-4" type="radio" name="options" defaultValue={1}>
-          <ToggleButton id="tbg-radio-1" value={1}>
+          <ToggleButton id="tbg-radio-1" value={1} onClick={() => this.setState({ category: fantasy })}>
             Fantasy
           </ToggleButton>
-          <ToggleButton id="tbg-radio-2" value={2}>
+          <ToggleButton id="tbg-radio-2" value={2} onClick={() => this.setState({ category: history })}>
             History
           </ToggleButton>
-          <ToggleButton id="tbg-radio-3" value={3}>
+          <ToggleButton id="tbg-radio-3" value={3} onClick={() => this.setState({ category: horror })}>
             Horror
           </ToggleButton>
-          <ToggleButton id="tbg-radio-3" value={3}>
+          <ToggleButton id="tbg-radio-4" value={4} onClick={() => this.setState({ category: romance })}>
             Romance
           </ToggleButton>
-          <ToggleButton id="tbg-radio-3" value={3}>
+          <ToggleButton id="tbg-radio-5" value={5} onClick={() => this.setState({ category: scifi })}>
             Scifi
           </ToggleButton>
         </ToggleButtonGroup>
-        <Row>
-          {fantasy.map((book) => {
+        <Row className="g-3">
+          {this.state.category.map((book) => {
             return (
               <Col xs={12} md={3} key={book.asin}>
-                <Card className="book-cover">
+                <Card>
                   <Card.Img variant="top" src={book.img} />
                   <Card.Body>
                     <Card.Title>{book.title}</Card.Title>
