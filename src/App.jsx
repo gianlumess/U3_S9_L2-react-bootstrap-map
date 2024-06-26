404eb99585d06962851c78a0919863905624d760
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-06-26 14:28:17 +0200

    edit navbar and deleted a component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import MyNav from "./components/MyNav";
-import ChooseBooksCategory from "./components/ChooseBooksCategory";
 import { Container } from "react-bootstrap";
 import MyFooter from "./components/MyFooter";
 import AllTheBooks from "./components/AllTheBooks";
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -3,7 +3,11 @@ import Alert from "react-bootstrap/Alert";
 
 class Welcome extends Component {
   render() {
-    return <Alert className="mt-4 text-center display-5">think of a book...find it!</Alert>;
+    return (
+      <Alert className="mt-4 text-center display-5">
+        think of a book...<strong>find it!</strong>
+      </Alert>
+    );
   }
 }
 
